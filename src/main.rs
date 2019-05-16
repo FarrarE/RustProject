b2d805539818085080e09a52c4b86d1d860ce093
Ezra <efarrar@pdx.edu>
2019-05-16 15:36:53 -0700

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,75 @@
+#![deny(missing_docs)]
 extern crate amethyst;
 
-mod game;
-mod systems;
+mod config;
+mod state;
+mod bundle;
+pub mod components;
+pub mod entities;
+pub mod systems;
+pub mod resources;
+
+pub use crate::config::GameConfiguration;
+pub use crate::config::GAME_CONFIGURATION;
+pub use crate::state::GameState;
+pub use crate::bundle::GameBundle;
 
 use amethyst::prelude::*;
-use amethyst::renderer::{DisplayConfig, DrawFlat2D,  
-                        Pipeline, RenderBundle, Stage};
 use amethyst::utils::application_root_dir;
 use amethyst::core::transform::TransformBundle;
+use amethyst::renderer::{DisplayConfig, RenderBundle, Pipeline, Stage, DrawFlat, PosTex};
+use amethyst::ui::{DrawUi, UiBundle};
 use amethyst::input::InputBundle;
-//use amethyst::ui::{DrawUi, UiBundle};
 
+// Used for the with_transparency()
+use gfx_core::state::{ColorMask};
+use gfx::preset::blend::ALPHA;
+
+const BACKGROUND_COLOUR: [f32; 4] = [0.25, 0.25, 0.25, 0.0];
+
+pub fn run() -> Result<(), amethyst::Error> {
+  let _ = &config::GAME_CONFIGURATION;
+  let application_root = application_root_dir();
+  let display_config_path =  format!("{}/resources/display_config.ron", application_root);
+  let display_config = DisplayConfig::load(&display_config_path);
+  let key_bindings_path = application_root.join("resources/bindings_config.ron");
+  let resources_path = format!("{}/texture", application_root);
+
+    let pipe = {
+      Pipeline::build()
+          .with_stage(
+              Stage::with_backbuffer()
+                  .clear_target(BACKGROUND_COLOUR, 1.0)
+                  .with_pass(DrawFlat::<PosTex>::new().with_transparency(ColorMask::all(), ALPHA, None))
+                  .with_pass(DrawUi::new())
+          )
+
+    };
+
+    let game_data = GameDataBuilder::default()
+    .with_bundle(
+        InputBundle::<String, String>::new().with_bindings_from_file(
+            &key_bindings_path
+        )?,
+    )?
+    .with_bundle(GameBundle)?
+    .with_bundle(TransformBundle::new())?
+    .with_bundle(UiBundle::<String, String>::new())?
+    .with_bundle(RenderBundle::new(pipe, Some(display_config)))?;
+
+    // Create a game with out game data and our GameState.
+    let mut game = Application::new(
+        resources_path,
+        GameState,
+        game_data)?;
+
+    Ok(
+        game.run(),
+    )
+
+}
+
+/*
 use crate::game::Game;
 
 fn main() -> amethyst::Result<()> {
@@ -50,4 +109,4 @@ fn main() -> amethyst::Result<()> {
     game.run();
 
     Ok(())
-}
+}*/
---
+++ src/state.rs
@@ -0,0 +1,42 @@
+use amethyst::prelude::*;
+use amethyst::input::{is_close_requested, is_key_down};
+use amethyst::winit::VirtualKeyCode;
+
+// use crate::entities::initialise_entities;
+// use crate::resources::add_resources;
+// use crate::components::register_components;
+// use crate::resources::{PlayState};
+
+pub struct GameState;
+
+
+impl SimpleState for GameState {
+    fn on_start(&mut self, state_data: StateData<'_, GameData<'_, '_>>) {
+        let world = state_data.world;
+        register_components(world);
+        add_resources(world);
+        initialise_entities(world);
+    }
+
+    fn handle_event(&mut self, _: StateData<'_, GameData<'_, '_>>, event: StateEvent) -> SimpleTrans{
+        if let  StateEvent::Window(event) = event {
+            if is_close_requested(&event) || is_key_down(&event, VirtualKeyCode::Escape) {
+                return Trans::Quit
+            }
+        }
+        Trans::None
+    }
+
+    // Stop the game if the ship runs out of lives
+    fn fixed_update(&mut self, state_data: StateData<'_, GameData<'_, '_>>) -> SimpleTrans{
+        let world = state_data.world;
+        let play_state = world.read_resource::<PlayState>();
+        if play_state.lives == 0 { Trans::Quit} else { Trans::None }
+    }
+
+    // This code tells Amethyst to run all the systems in your game data.
+    fn update (&mut self, state_data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans{
+        state_data.data.update(&state_data.world);
+        Trans::None
+    }
+}
