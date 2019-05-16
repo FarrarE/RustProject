488986f20ea92b45146ae39d9f1ac9d64e2e9387
Ezra <efarrar@pdx.edu>
2019-05-16 13:10:09 -0700

    Added projectile component and entity

---
+++ src/Component/projectile.rs
@@ -0,0 +1,15 @@
+use amethyst::ecs::prelude::{Component, DenseVecStorage};
+
+/// Component for our asteroid-destroying laser beam.
+#[derive(Clone)]
+pub struct Projectile {
+    /// The speed of our laser beam
+    pub velocity: f32,
+    /// The width of our laser beam
+    pub width: f32,
+    /// The height of our laser beam
+    pub height: f32,
+}
+
+impl Component for Projectile {
+  
