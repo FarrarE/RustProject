513b0096756942f2d2fae8f7f289a1bba1dc05b8
Mark Williams <markw676@gmail.com>
2019-05-26 15:27:48 -0700

    Mixed up cos and sin

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -7,5 +7,5 @@ pub use self::{player::PlayerSystem, projectile::ProjectileSystem};
 //Returns the unit vector between a start and end point.
 pub fn get_vector(start: (f32, f32), end: (f32, f32), velocity: f32 ) -> (f32 , f32) {
     let theta = f32::atan(start.0 - end.0 / start.1 - end.1);
-    return ((velocity * f32::sin(theta)), (velocity * f32::cos(theta)) );
+    return ((velocity * f32::cos(theta)), (velocity * f32::sin(theta)) );
 }
