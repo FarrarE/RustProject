bb7578c5cf8ed12b9ae100d85248c7dde2f55e40
Ian <imcbride@pdx.edu>
2019-05-26 17:11:16 -0700

    Initial movement pass
    need to tweak the get_vector method to account for signs

--- src/systems/enemy.rs
+++ src/systems/enemy.rs
@@ -33,6 +33,7 @@ impl<'s> System<'s> for EnemySystem {
       for (transform, enemy) in (&mut transforms, &enemies).join() {
         let start = (transform.translation().x, transform.translation().y);
         let end = (player_x, player_y);
+
         let (x, y) = get_vector(start, end);
         transform.translate_x(x * enemy.velocity * time.delta_seconds());
         transform.translate_y(y * enemy.velocity * time.delta_seconds());
@@ -45,7 +46,7 @@ impl<'s> System<'s> for EnemySystem {
                 .max(0.0)
                 .min(ARENA_HEIGHT - enemy.height),
         );
-        
+
         let enemy_x = transform.translation().x;
         transform.set_x(
             enemy_x
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -8,10 +8,16 @@ mod spawn;
 pub use self::{player::PlayerSystem, projectile::ProjectileSystem, enemy::EnemySystem, spawn::SpawnSystem};
 
 //Returns the unit vector between a start and end point.
+<<<<<<< HEAD
 pub fn get_vector(start: (f32, f32), end: (f32, f32)) -> (f32 , f32) {
     let dx = end.0 - start.0;
     let dy = end.1 - start.1;
     let mag = f32::sqrt(dx.powi(2) + dy.powi(2));
 
     (dx / mag, dy / mag)
+=======
+pub fn get_vector(start: (f32, f32), end: (f32, f32), velocity: f32 ) -> (f32 , f32) {
+    let theta = f32::atan(start.0 - end.0 / start.1 - end.1);
+    ((velocity * f32::cos(theta)), (velocity * f32::sin(theta)))
+>>>>>>> Initial movement pass
 }
